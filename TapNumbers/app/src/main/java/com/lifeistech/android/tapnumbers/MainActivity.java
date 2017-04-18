1c82d46bc1ef8f5dfc74beb8259f533c158fda1b
Watarai Hitoe <bkht1411389@gmail.com>
2017-04-18 11:31:56 +0900

    add TapNumbersApp

---
+++ TapNumbers/app/src/main/java/com/lifeistech/android/tapnumbers/MainActivity.java
@@ -0,0 +1,95 @@
+package com.lifeistech.android.tapnumbers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    int[] hairetu;
+    String mondai;
+    int seiseki;
+    TextView textView;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        textView = (TextView) findViewById(R.id.textView);
+        start();
+    }
+    
+    public void start(){
+
+        hairetu = new int[4];
+        Random rand = new Random();
+        hairetu[0] = rand.nextInt(4) + 1;
+        hairetu[1] = rand.nextInt(4) + 1;
+        hairetu[2] = rand.nextInt(4) + 1;
+        hairetu[3] = rand.nextInt(4) + 1;
+
+        mondai = Integer.toString(hairetu[0])
+                  + Integer.toString(hairetu[1])
+                  + Integer.toString(hairetu[2])
+                  + Integer.toString(hairetu[3]);
+        textView.setText(mondai);
+        seiseki=0;
+    }
+
+    public void number1(View v){
+        if(hairetu[seiseki] == 1){
+            mondai = mondai.substring(1);
+            textView.setText(mondai);
+            seiseki = seiseki + 1;
+            if(seiseki == 4){
+                start();
+            }
+        }else{
+            Toast.makeText(this, "数字が違うよ！！", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void number2(View v){
+        if(hairetu[seiseki] == 2){
+            mondai = mondai.substring(1);
+            textView.setText(mondai);
+            seiseki = seiseki + 1;
+            if(seiseki == 4){
+                start();
+            }
+        }else{
+            Toast.makeText(this, "数字が違うよ！！", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void number3(View v){
+        if(hairetu[seiseki] == 3){
+            mondai = mondai.substring(1);
+            textView.setText(mondai);
+            seiseki = seiseki + 1;
+            if(seiseki == 4){
+                start();
+            }
+        }else{
+            Toast.makeText(this, "数字が違うよ！！", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void number4(View v){
+        if(hairetu[seiseki] == 4){
+            mondai = mondai.substring(1);
+            textView.setText(mondai);
+            seiseki = seiseki + 1;
+            if(seiseki == 4){
+                start();
+            }
+        }else{
+            Toast.makeText(this, "数字が違うよ！！", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
